ad16d51c1527e29a827654123e43a09ec9ea033f
kathryn0908 <kathrynlael@gmail.com>
2020-08-28 15:15:53 -0600

    complete intro practice for file setup and PersonModel creation

---
+++ IntroUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel
+            {
+                FirstName = "Tim",
+                LastName = "Corey"
+            };
+            Console.WriteLine($"{p.FirstName} {p.LastName}");
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("This is a test");
+        }
+    }
+}
